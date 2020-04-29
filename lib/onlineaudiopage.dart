43b253f688e79a6962d1614b5180e399d9725dc6
kuldipsinh09 <kuldipsinh.mscit15@gmail.com>
2020-04-29 17:40:26 +0530

    Add Add url straem library and apply stream

--- lib/onlineaudiopage.dart
+++ lib/onlineaudiopage.dart
@@ -1,10 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
+import 'package:flute_music_player/flute_music_player.dart';
 import 'package:flutter/material.dart';
-import 'package:url_audio_stream/url_audio_stream.dart';
+
+enum PlayerState { stopped, playing, paused }
 
 class OnlineAudioPlay extends StatefulWidget {
   @override
-  _OnlineAudioPlayState createState() => _OnlineAudioPlayState();
+  _OnlineAudioPlayState createState() => new _OnlineAudioPlayState();
 }
 
 class _OnlineAudioPlayState extends State<OnlineAudioPlay> {
@@ -13,85 +15,222 @@ class _OnlineAudioPlayState extends State<OnlineAudioPlay> {
     "https://luan.xyz/files/audio/nasa_on_a_mission.mp3",
     "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1xtra_mf_p"
   ];
-  double _sliderValue = 0.0;
+
+  Duration duration;
+  Duration position;
+
+  MusicFinder audioPlayer;
 
   String localFilePath;
-  AudioStream stream;
-  bool _isResume = false;
+
+  PlayerState playerState = PlayerState.stopped;
+
+  get isPlaying => playerState == PlayerState.playing;
+  get isPaused => playerState == PlayerState.paused;
+
+  get durationText =>
+      duration != null ? duration.toString().split('.').first : '';
+  get positionText =>
+      position != null ? position.toString().split('.').first : '';
+
+  bool isMuted = false;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-//    _loadFile(arrayAudioURL[0]);
-    stream = new AudioStream(arrayAudioURL[0]);
-
+    initAudioPlayer();
   }
+
+  Future initAudioPlayer() async {
+    audioPlayer = new MusicFinder();
+    var songs;
+    try {
+      songs = await MusicFinder.allSongs();
+    } catch (e) {
+      print(e.toString());
+    }
+
+    print(songs);
+    audioPlayer.setDurationHandler((d) => setState(() {
+      duration = d;
+    }));
+
+    audioPlayer.setPositionHandler((p) => setState(() {
+      position = p;
+    }));
+
+    audioPlayer.setCompletionHandler(() {
+      onComplete();
+      setState(() {
+        position = duration;
+      });
+    });
+
+    audioPlayer.setErrorHandler((msg) {
+      setState(() {
+        playerState = PlayerState.stopped;
+        duration = new Duration(seconds: 0);
+        position = new Duration(seconds: 0);
+      });
+    });
+
+    setState(() {
+      print(songs.toString());
+    });
+  }
+
+  Future play() async {
+    final result = await audioPlayer.play(arrayAudioURL[0]);
+    if (result == 1)
+      setState(() {
+        print('_AudioAppState.play... PlayerState.playing');
+        playerState = PlayerState.playing;
+      });
+  }
+
+  Future _playLocal() async {
+    final result = await audioPlayer.play(localFilePath, isLocal: true);
+    if (result == 1) setState(() => playerState = PlayerState.playing);
+  }
+
+  Future pause() async {
+    final result = await audioPlayer.pause();
+    if (result == 1) setState(() => playerState = PlayerState.paused);
+  }
+
+  Future stop() async {
+    final result = await audioPlayer.stop();
+    if (result == 1)
+      setState(() {
+        playerState = PlayerState.stopped;
+        position = new Duration();
+      });
+  }
+
+  Future mute(bool muted) async {
+    final result = await audioPlayer.mute(muted);
+    if (result == 1)
+      setState(() {
+        isMuted = muted;
+      });
+  }
+
+  void onComplete() {
+    setState(() => playerState = PlayerState.stopped);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    audioPlayer.stop();
+  }
+
+//  Future<Uint8List> _loadFileBytes(String url, {OnError onError}) async {
+//    Uint8List bytes;
+//    try {
+//      bytes = await readBytes(url);
+//    } on ClientException {
+//      rethrow;
+//    }
+//    return bytes;
+//  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Audio player"),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(20),
-        alignment: Alignment.center,
-        child: Column(
+    return new Center(
+        child: new Material(
+            elevation: 2.0,
+            color: Colors.grey[200],
+            child: new Center(
+              child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    new Material(child: _buildPlayer()),
+                    localFilePath != null
+                        ? new Text(localFilePath)
+                        : new Container(),
+                    new Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: new Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            new RaisedButton(
+                              onPressed: () => {},
+                              child: new Text('Download'),
+                            ),
+                            new RaisedButton(
+                              onPressed: () => _playLocal(),
+                              child: new Text('play local'),
+                            ),
+                          ]),
+                    )
+                  ]),
+            )));
+  }
+
+  Widget _buildPlayer() => new Container(
+      padding: new EdgeInsets.all(16.0),
+      child: new Column(mainAxisSize: MainAxisSize.min, children: [
+        new Row(mainAxisSize: MainAxisSize.min, children: [
+          new IconButton(
+              onPressed: isPlaying ? null : () => play(),
+              iconSize: 64.0,
+              icon: new Icon(Icons.play_arrow),
+              color: Colors.cyan),
+          new IconButton(
+              onPressed: isPlaying ? () => pause() : null,
+              iconSize: 64.0,
+              icon: new Icon(Icons.pause),
+              color: Colors.cyan),
+          new IconButton(
+              onPressed: isPlaying || isPaused ? () => stop() : null,
+              iconSize: 64.0,
+              icon: new Icon(Icons.stop),
+              color: Colors.cyan),
+        ]),
+        duration == null
+            ? new Container()
+            : new Slider(
+            value: position?.inMilliseconds?.toDouble() ?? 0,
+            onChanged: (double value) =>
+                audioPlayer.seek((value / 1000).roundToDouble()),
+            min: 0.0,
+            max: duration.inMilliseconds.toDouble()),
+        new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            SizedBox(height: 30),
-            Text(localFilePath != null ? localFilePath : "Song title"),
-            slider(),
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                    icon: Image.asset("images/next.png"), onPressed: () {}),
-                SizedBox(
-                  width: 40,
-                ),
-                IconButton(
-                    icon: Image.asset(_isResume ? "images/pause.png" : "images/play.png"), onPressed: () {
-                  setState(() {
-                    _isResume = !_isResume;
-                  });
-                  if (_isResume) {playMusic();} else {resumeMusic();}
-                }),
-                SizedBox(
-                  width: 40,
-                ),
-                IconButton(
-                    icon: Image.asset("images/previous.png"), onPressed: () {})
-                ,
-              ],
-            )
+            new IconButton(
+                onPressed: () => mute(true),
+                icon: new Icon(Icons.headset_off),
+                color: Colors.cyan),
+            new IconButton(
+                onPressed: () => mute(false),
+                icon: new Icon(Icons.headset),
+                color: Colors.cyan),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget slider() {
-    return Slider(
-      value: _sliderValue,
-      min: 0.0,
-      max: 10.0,
-      onChanged: (newSliderValue) {
-        setState(() {
-          _sliderValue = newSliderValue;
-        });
-      },
-    );
-  }
-
-
-  void playMusic() async {
-    stream.start();
-  }
-  void resumeMusic()  {
-    stream.pause();
-    // quickly plays the sound, will not release
-  }
+        new Row(mainAxisSize: MainAxisSize.min, children: [
+          new Padding(
+              padding: new EdgeInsets.all(12.0),
+              child: new Stack(children: [
+                new CircularProgressIndicator(
+                    value: 1.0,
+                    valueColor: new AlwaysStoppedAnimation(Colors.grey[300])),
+                new CircularProgressIndicator(
+                  value: position != null && position.inMilliseconds > 0
+                      ? (position?.inMilliseconds?.toDouble() ?? 0.0) /
+                      (duration?.inMilliseconds?.toDouble() ?? 0.0)
+                      : 0.0,
+                  valueColor: new AlwaysStoppedAnimation(Colors.cyan),
+                  backgroundColor: Colors.yellow,
+                ),
+              ])),
+          new Text(
+              position != null
+                  ? "${positionText ?? ''} / ${durationText ?? ''}"
+                  : duration != null ? durationText : '',
+              style: new TextStyle(fontSize: 24.0))
+        ])
+      ]));
 }
